9bddf29f08c0c6c99fa9a9799aed4b73855ef544
FIinRo <82387041+FIinRo@users.noreply.github.com>
2024-08-06 13:09:19 +0300

    .

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,32 @@
+package pages;
+
+import helperMethods.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+import java.awt.*;
+
+public class BasePage {
+
+    protected WebDriver driver;
+    protected ElementMethods elementMethods;
+    protected PageMethods pageMethods;
+    protected AlertMethods alertMethods;
+    protected FrameMethods frameMethods;
+    protected WindowMethods windowMethods;
+
+
+    public BasePage (WebDriver driver) {
+        this.driver = driver;
+        pageMethods = new PageMethods(driver);
+        elementMethods = new ElementMethods (driver);
+        alertMethods=new AlertMethods(driver);
+        frameMethods=new FrameMethods(driver);
+        windowMethods=new WindowMethods(driver);
+        PageFactory.initElements(driver, this);
+    }
+
+
+
+
+}
