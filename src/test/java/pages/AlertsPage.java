3121f65af57257041428eee059a6e1e874fc75cf
FIinRo <82387041+FIinRo@users.noreply.github.com>
2024-08-06 18:26:32 +0300

    .

--- src/test/java/pages/AlertsPage.java
+++ src/test/java/pages/AlertsPage.java
@@ -3,23 +3,16 @@ package pages;
 import helperMethods.AlertMethods;
 import helperMethods.ElementMethods;
 import helperMethods.PageMethods;
+import loggerUtility.LoggerUtility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class AlertsPage {
-    public WebDriver driver;
-    public ElementMethods elementMethods;
-    public PageMethods pageMethods;
-    public AlertMethods alertMethods;
+public class AlertsPage extends BasePage{
 
     public AlertsPage(WebDriver driver) {
-        this.driver = driver;
-        elementMethods = new ElementMethods(driver);
-        pageMethods = new PageMethods(driver);
-        alertMethods = new AlertMethods(driver);
-        PageFactory.initElements(driver, this);
+        super(driver);
     }
 
     @FindBy(id = "alertButton")
@@ -31,24 +24,34 @@ public class AlertsPage {
     @FindBy(id = "timerAlertButton")
     public WebElement timerAlertButton;
 
+
+
     public void interactAlertOk(){
         elementMethods.clickElement(firstAlert);
+        LoggerUtility.info("The user clicks on the alertOK button");
         alertMethods.acceptAlert();
+        LoggerUtility.info("The user interacts with the alertOK successfully");
     }
 
     public void interactWithConfirmationButton(){
         elementMethods.clickElement(confirmAlertButton);
+        LoggerUtility.info("The user clicks on the confirmAlert button");
         alertMethods.dismissAlert();
+        LoggerUtility.info("The user interacts with the confirmAlert successfully");
     }
 
     public void interactWithPromptButton(String text){
         elementMethods.clickElement(promptAlertButton);
+        LoggerUtility.info("The user clicks on the prompt button");
         alertMethods.fillAlert(text);
+        LoggerUtility.info("The user submits the text in the prompt");
     }
 
     public void interactWithTimerButton(){
         elementMethods.clickElement(timerAlertButton);
+        LoggerUtility.info("The user clicks on the alertDelay button");
         alertMethods.acceptAlert();
+        LoggerUtility.info("The user interacts with the alertDelay successfully");
     }
 
 }
--- src/test/java/pages/AlertsWindowsPage.java
+++ src/test/java/pages/AlertsWindowsPage.java
@@ -2,21 +2,16 @@ package pages;
 
 import helperMethods.ElementMethods;
 import helperMethods.PageMethods;
+import loggerUtility.LoggerUtility;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class AlertsWindowsPage {
-    public WebDriver driver;
-    public ElementMethods elementMethods;
-    public PageMethods pageMethods;
+public class AlertsWindowsPage extends BasePage{
 
     public AlertsWindowsPage(WebDriver driver) {
-        this.driver = driver;
-        elementMethods = new ElementMethods(driver);
-        pageMethods = new PageMethods(driver);
-        PageFactory.initElements(driver, this);
+        super(driver);
     }
 
     @FindBy(xpath = "//span[text()='Browser Windows']")
@@ -26,19 +21,23 @@ public class AlertsWindowsPage {
     @FindBy(xpath = "//span[text()='Alerts']")
     public WebElement alertsFromSubmenu;
 
+
     public void navigateToWindowPage(){
         pageMethods.scrollPage(0,350);
         elementMethods.clickElement(browserWindowsSubmenu);
+        LoggerUtility.info("The user clicks on Form submenu");
     }
 
     public void navigateToFramePage(){
         pageMethods.scrollPage(0, 350);
         elementMethods.clickElement(framesFromSubmenu);
+        LoggerUtility.info("The user clicks on Frame page");
 
     }
 
     public void navigateToAlertsPage(){
         elementMethods.clickElement(alertsFromSubmenu);
+        LoggerUtility.info("The user clicks on alerts form");
     }
 
 }
